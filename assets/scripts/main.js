820a693e949ebe28c81ecf86d5cbfcbb94cdb4b7
Sankarsan Kampa <me@sankarsankampa.com>
2018-01-26 18:29:20 +0000

    Autofetch Bastion bot status

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,3 +5,18 @@ sp.summary({
     for (let component of components) document.getElementById(`discord:${component.name.toLowerCase().replace(/ /g, '-')}`).innerHTML = component.status.charAt(0).toUpperCase() + component.status.substr(1).toLowerCase();
   }
 });
+
+$.ajax({
+  url: 'https://discordbots.org/api/bots/267035345537728512/status',
+  dataType: 'json',
+  error: (response, type, exception) => {
+    console.error({
+      exception: exception,
+      type: type,
+      response: response
+    });
+  },
+  success: (bastion) => {
+    document.getElementById('bastion:bot').innerHTML = bastion.status.charAt(0).toUpperCase() + bastion.status.substr(1).toLowerCase();
+  }
+});
