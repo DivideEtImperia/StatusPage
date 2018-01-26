502b29bbf804f0442c0f828ca56c304e4b56a601
Sankarsan Kampa <me@sankarsankampa.com>
2018-01-26 17:57:48 +0000

    Moved scripts to external file

---
+++ assets/scripts/main.js
@@ -0,0 +1,7 @@
+let sp = new StatusPage.page({ page : 'srhpyqt94yxb' });
+sp.summary({
+  success : (data) => {
+    let components = data.components.filter(component => /(API|Gateway|CloudFlare|Media Proxy)/i.test(component.name));
+    for (let component of components) document.getElementById(`discord:${component.name.toLowerCase().replace(/ /g, '-')}`).innerHTML = component.status.charAt(0).toUpperCase() + component.status.substr(1).toLowerCase();
+  }
+});
